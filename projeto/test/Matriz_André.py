655533d8d4f4e3c2efccc2a5aa66f825ccd8c61e
Andre Casimiro <andrecasimiro60@gmail.com>
2023-03-20 00:19:00 -0300

    Matriz André

---
+++ projeto/test/Matriz_André.py
@@ -0,0 +1,15 @@
+mundo = [
+        [60, 60, 1, 1],
+        [60, 1, 10, 10],
+        [1, 1, 10, 10],
+        [1, 10, 10, 10]]
+
+for i in range(4):
+    for j in range(4):
+        if mundo[i][j] == 60:
+            print('-montanha- 60 pontos', end = " ")
+        elif mundo[i][j] == 10:
+            print('-água- 10 pontos', end = " ")
+        elif mundo[i][j] == 1:
+            print('-Grama- 1 ponto', end = " ")
+    print()
