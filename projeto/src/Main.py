36a92364b1b67927028d831e1d133b9a9e2f5484
Bruno José <bruninhosese12@gmail.com>
2023-03-26 20:19:26 -0300

    atualizando o print

--- projeto/src/Main.py
+++ projeto/src/Main.py
@@ -1,4 +1,5 @@
 from Agente import Agente
+import time as t
 
 agente = Agente()
 
@@ -7,12 +8,21 @@ agente = Agente()
 # para trabalhar com o agente, utilize os métodos deslocar() e mostrarMundo()
 agente.deslocar()
 agente.mostrarMundo()
+t.sleep(1)
+
+print("\033c", end="")
 
 agente.deslocar()
 agente.mostrarMundo()
+t.sleep(1)
+
+print("\033c", end="")
 
 agente.deslocar()
 agente.mostrarMundo()
+t.sleep(1)
+
+print("\033c", end="")
 
 agente.deslocar()
 agente.mostrarMundo()
