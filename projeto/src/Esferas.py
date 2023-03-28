065d9fae479fbaf889a97492f5403888fc2b48f9
Bruno José <bruninhosese12@gmail.com>
2023-03-28 09:12:27 -0300

    ajuste de classe

--- projeto/src/Esferas.py
+++ projeto/src/Esferas.py
@@ -1,18 +1,19 @@
 import random
 
 class Esferas:
+
     localizacoes = []
 
-    def __init__ (self):
+    def __init__(self):
         self.sorteador = random
 
-        self.ESFERA1 ='Esfera 1'
-        self.ESFERA2 ='Esfera 2'
-        self.ESFERA3 ='Esfera 3'
-        self.ESFERA4 ='Esfera 4'
-        self.ESFERA5 ='Esfera 5'
-        self.ESFERA6 ='Esfera 6'
-        self.ESFERA7 ='Esfera 7'
+        self.ESFERA1 = 'Esfera 1'
+        self.ESFERA2 = 'Esfera 2'
+        self.ESFERA3 = 'Esfera 3'
+        self.ESFERA4 = 'Esfera 4'
+        self.ESFERA5 = 'Esfera 5'
+        self.ESFERA6 = 'Esfera 6'
+        self.ESFERA7 = 'Esfera 7'
     
         self.esferas = {	
             self.ESFERA1,
@@ -26,18 +27,17 @@ class Esferas:
         
         self.gerarLocalizacoes()
 
-    def gerarLocalizacoes (self):
+
+    def gerarLocalizacoes(self):
+
         for _ in self.esferas:
             self.localizacoes.append(
                 [
-                    self.eixo(),
-                    self.eixo(),
+                    self.sortearPosicaoNoEixo(),
+                    self.sortearPosicaoNoEixo(),
                     True
                 ])
 
 
-    def eixo (self):
+    def sortearPosicaoNoEixo(self):
         return self.sorteador.randrange(42)
-
-# esfera = Esferas()
-# print(esfera.localizacoes)
